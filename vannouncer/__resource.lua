efc313aeae3b6fde8b3f4ef2c76933c03e95e6fe
Tom Grobbe <31419184+TomGrobbe@users.noreply.github.com>
2017-10-08 23:19:51 +0200

    Updated resource manifest
    Updated resource manifest to the latest version.

--- vannouncer/__resource.lua
+++ vannouncer/__resource.lua
@@ -1,3 +1,3 @@
-resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 client_script "va_client.lua"
 server_script "va_server.lua"
